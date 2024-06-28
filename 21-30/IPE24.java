0c8eb97ae0fca605f34328eb8656be31bc3d7a6e
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-06-28 18:26:52 +0530

    add

--- 21-30/IPE24.java
+++ 21-30/IPE24.java
@@ -1,3 +1,20 @@
-public class IPE24 {
+import java.util.Scanner;
 
+public class IPE24 {
+    public static void main(String[] args) {
+        // WAP to find 1+1/2+1/3+1/4+………+1/N series.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number ");
+        int num = sc.nextInt();
+        double sum = 0.0;
+        int numerator = 1;
+        int denominator = 1;
+        for (int i = 1; i <= num; i++) {
+            sum += (double) (numerator / denominator);
+            System.out.println(numerator + " " + denominator);
+            denominator++;
+        }
+        System.out.println("sum = " + sum);
+        sc.close();
+    }
 }
--- 31-40/IPE31.java
+++ 31-40/IPE31.java
@@ -2,12 +2,31 @@ import java.util.Scanner;
 
 public class IPE31 {
 
+    public static boolean perfectNum(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == num;
+    }
+
     public static void main(String[] args) {
         // 31 WAP to check whether a number is a perfect number or not. (e.g. 123 is a
         // perfect no i.e.1+2+3=1*2*3)
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number to check prime or not : ");
+        System.out.println("Enter Number to check perfect or not : ");
         int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            if (perfectNum(i)) {
+                System.out.print(i + " ");
+            }
+        }
 
+        sc.close();
     }
 }
