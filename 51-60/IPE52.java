3b8c0c05fa8f8b665c90a12baaf8618f90b8f01d
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-06-28 18:37:01 +0530

    Update IPE52.java

--- 51-60/IPE52.java
+++ 51-60/IPE52.java
@@ -8,6 +8,15 @@ public class IPE52 {
         return gcd(b, a % b);
     }
 
+    public static int gcd1(int a, int b) {
+        if (a == b) {
+            return a;
+        } else if (a > b) {
+            return gcd1(a - b, b);
+        }
+        return gcd1(a, b - a);
+    }
+
     public static void main(String[] args) {
         // 52 WAP to find GCD of the 2 numbers using recursion.
         Scanner sc = new Scanner(System.in);
@@ -17,6 +26,8 @@ public class IPE52 {
         int num2 = sc.nextInt();
 
         System.out.println("The GCD of " + num1 + " and " + num2 + " is: " + gcd(num1, num2));
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is: " + gcd1(num1, num2));
+        sc.close();
     }
 
 }
